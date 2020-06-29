a1c7a276fbd30ef124170e8d6f8834f869b29b62
Rien Boydens <rien.boydens@gmail.com>
2020-06-29 12:31:41 +0200

    Finished playable game

---
+++ connect4/__main__.py
@@ -0,0 +1,71 @@
+from blessed import Terminal
+from . import c4
+
+import sys
+import random
+
+t = Terminal()
+
+with t.location():
+    print('Starting game...')
+
+
+pcolor = {
+    1: t.blue,
+    2: t.red
+}
+
+
+def get_input(player):
+    col = None
+    print(f'{pcolor[cur_player](f"P{cur_player}")}>>', end="")
+
+    with t.cbreak():
+        while True:
+            key = t.inkey()
+            
+            if key == '\x03': # Ctrl+C
+                raise KeyboardInterrupt
+
+            if key.is_sequence and key.code == t.KEY_ENTER and col is not None:
+                if c4.put_disc(player, col):
+                    break
+            
+            if not key.is_sequence and key in (str(x+1) for x in range(7)):
+                col = int(key)
+            
+            if col is not None:
+                print(col, end="\b")
+
+
+def print_game():
+    print(t.clear_eos)
+    print(status_message)
+    print("=" * len(status_message))
+    c4.print_board()
+
+        
+try:
+
+    cur_player = random.randint(1, 2)
+
+    status_message = f'{pcolor[cur_player](f"Player {cur_player}")} begins.'
+
+    with t.location():
+        print_game()
+        get_input(cur_player)
+
+    while not c4.check_win(cur_player):
+        cur_player = 1 - (cur_player-1) + 1
+        with t.location():
+            status_message = f'{pcolor[cur_player](f"Player {cur_player}")}\'s turn'
+            print_game()
+            get_input(cur_player)
+
+    status_message = f'{t.bold(pcolor[cur_player](f"Player {cur_player}"))} won the game!'
+    print_game()
+    print()
+
+except KeyboardInterrupt:
+    print_game()
+    print('\nGame interupted by KeyboardInterrupt')
---
+++ connect4/c4.py
@@ -0,0 +1,58 @@
+from blessed import Terminal
+from IPython import embed
+
+
+COLUMNS = 7
+ROWS = 6
+
+t = Terminal()
+
+board = [[0 for column in range(COLUMNS)] for row in range(ROWS)]
+signs = {
+    0: t.white('O'),
+    1: t.blue('O'),
+    2: t.red('O')
+}
+
+
+def put_disc(player, column):
+    row = -1
+    column -= 1
+    while board[row][column]:
+        row -= 1
+        if row < -ROWS:
+            return False
+    board[row][column] = player
+    return True
+
+
+def check_win(player):
+    def check_pos(row, column):
+        for dx, dy in ((1, -1), (1, 0), (1, 1), (0, 1)):
+            count = 0
+            for scale in range(4):
+                if (row + dx*scale >= ROWS or column + dy*scale >= COLUMNS): continue
+                if board[row + dx * scale][column + dy * scale] == player:
+                    count += 1
+                    if count == 4:
+                        return True
+        return False
+
+    for row in range(ROWS):
+        for column in range(COLUMNS):
+            if board[row][column] != player: continue
+            if check_pos(row, column):
+                return True
+    return False
+                    
+
+def print_board():
+    print(t.bold("1 2 3 4 5 6 7 "))
+    for row in board:
+        for cell in row:
+            print(signs[cell], end=" ")
+        print()
+
+
+if __name__=='__main__':
+    embed()
